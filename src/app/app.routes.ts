9163dc1e1d7d91e22f77e3652f9557dedba8dac3
AAAnjalz <adhikarinjal2062@gmail.com>
2026-09-14 15:02:36 -0500

    added route fixes for redirecting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'personal-portfolio',
+    component: App,
+  },
+  {
+    path: '**',
+    redirectTo: 'personal-portfolio',
+  },
+];
